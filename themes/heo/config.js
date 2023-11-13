8a71a26278d7663f099ad383a14f9ad50e33e075
Cherryhua <133014375+Cherryhua69@users.noreply.github.com>
2023-11-13 16:17:03 +0800

    Update config.js 主题修改heo

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-11-13', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的Blog', url: 'https://blog.cherrylord.cn' },
+    { title: '使用ChatGPT请点击这(需要使用请咨询邮箱1660503966@qq.com)', url: 'https://ai.cherrylord.cn' }
   ],
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '上线两年半',
+  HEO_HERO_TITLE_5: '欢迎阅览本网站！',
+  HEO_HERO_TITLE_LINK: 'https://blog.cherrylord.cn',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -37,7 +37,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/Cherryhua69', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
